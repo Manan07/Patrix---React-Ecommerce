e859d73e7c0e42a9c829a740ebd6ba5597557eaf
Manan Chaturvedi <mananchaturvedic07@gmail.com>
2021-09-13 01:00:50 +0530

    Homepage & Menu item components
    Divided homepage into smaller components and styled menu item components

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Directory from "../../components/directory/directory.components";
+
+import "./homepage.styles.css";
+
+export const HomePage = () => (
+  <div className="Homepage">
+    <Directory />
+  </div>
+);
