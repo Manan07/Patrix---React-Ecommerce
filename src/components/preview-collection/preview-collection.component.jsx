409b462fec11fb73474f5a270c0292aa52a50dba
Manan Chaturvedi <mananchaturvedic07@gmail.com>
2021-11-19 13:48:19 +0530

    Shop Page and collection items

---
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import CollectionItem from "../collection-item/collection-item.component";
+
+// Style
+import "./preview-collection.component.css";
+
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1>{title.toUpperCase()}</h1>
+
+    <div className="preview">
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
+    </div>
+  </div>
+);
+
+export default CollectionPreview;
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+// Imports
+import SHOP_DATA from "./shop.data";
+import CollectionPreview from "../../components/preview-collection/preview-collection.component";
+
+class ShopPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className={ShopPage}>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
