1e5cd13aa4e17993f1a261136eb9dfda61faedb8
Manan Chaturvedi <mananchaturvedic07@gmail.com>
2021-11-21 16:37:08 +0530

    Firebase - SignIn/Out

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { auth } from "../../firebase/firebase.utils";
 // Styles
 import "./header.styles.css";
 
 // Logo
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
-const Header = () => {
+const Header = ({ currentUser }) => {
   return (
     <div className="header">
       <Link className="logo-container" to="/">
@@ -21,6 +22,20 @@ const Header = () => {
         <Link className="option" to="/">
           Contact
         </Link>
+        {currentUser ? (
+          <div
+            className="option"
+            onClick={() => {
+              auth.signOut();
+            }}
+          >
+            Sign Out
+          </div>
+        ) : (
+          <Link className="option" to="/signin">
+            Sign In
+          </Link>
+        )}
       </div>
     </div>
   );
