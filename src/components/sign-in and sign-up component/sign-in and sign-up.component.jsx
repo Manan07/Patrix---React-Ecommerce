6010cef10cf1a8caaa250164d23b091bba6e984e
Manan Chaturvedi <mananchaturvedic07@gmail.com>
2021-11-22 12:50:29 +0530

    Firebase
    Implemented Firebase utils, added Signup

--- src/components/sign-in and sign-up component/sign-in and sign-up.component.jsx
+++ src/components/sign-in and sign-up component/sign-in and sign-up.component.jsx
@@ -5,10 +5,12 @@ import "./sign-in and sign-up.style.css";
 
 // Components
 import SignIn from "../Sign-in/sign-in.component";
+import SignUp from "../sign-up/sign-up.component";
 
 const SignInAndSignUp = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
